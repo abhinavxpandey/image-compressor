4c8df56ef0af2554fecffae15fc9ca945ed2f830
abhinavxpandey <pandeyabhinavhero6@gmail.com>
2026-09-19 15:19:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import multer from "multer";
+import sharp from "sharp";
+import cors from "cors";
+const PORT = process.env.PORT || 3000;
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 50 * 1024 * 1024 }
+
+});
+
+const app= express();
+app.use(cors());
+
+app.post("/upload",upload.single("image"),async (req,res)=>{
+try{
+if(!req.file){
+    res.status(400).json({error: "please upload a file"});
+}
+const OriginalImage = req.file.buffer;
+
+const CompressedImage= await sharp(OriginalImage)
+.resize({
+    width:1600,
+    withoutEnlargement:true,
+})
+.jpeg({
+    quality:80,
+})
+.toBuffer();
+
+res.type("image/jpeg").send(CompressedImage);
+
+} catch(error){
+    console.error(error);
+
+    res.status(500).json({error: "failed to proceed"});
+}
+
+} );
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ script.js
@@ -0,0 +1,88 @@
+console.log("Script initialized");
+
+const imageInput = document.getElementById("imageInput");
+const originalPreview = document.getElementById("originalPreview");
+const compressBtn = document.getElementById("compressBtn");
+
+const resultEmpty = document.getElementById("resultEmpty");
+const resultBox = document.getElementById("resultBox");
+
+const compressedPreview = document.getElementById("compressedPreview");
+const originalSize = document.getElementById("originalSize");
+const compressedSize = document.getElementById("compressedSize");
+const downloadLink = document.getElementById("downloadLink");
+
+
+
+imageInput.addEventListener("change", () => {
+
+    const image = imageInput.files[0];
+
+    if (!image) {
+        return;
+    }
+
+    
+    originalPreview.src = URL.createObjectURL(image);
+    originalPreview.classList.remove("hidden");
+
+    
+    originalSize.textContent = `Original: ${formatSize(image.size)}`;
+});
+
+
+
+compressBtn.addEventListener("click", async () => {
+
+    const image = imageInput.files[0];
+
+    if (!image) {
+        alert("Please select an image first.");
+        return;
+    }
+
+   
+    const formData = new FormData();
+
+    formData.append("image", image);
+
+
+   
+    const response = await fetch("http://localhost:3000/upload", {
+        method: "POST",
+        body: formData
+    });
+
+    
+    const compressedImage = await response.blob();
+
+
+    
+    const compressedURL = URL.createObjectURL(compressedImage);
+
+    compressedPreview.src = compressedURL;
+
+    
+    resultEmpty.classList.add("hidden");
+    resultBox.classList.remove("hidden");
+
+
+    
+    compressedSize.textContent =
+        `Compressed: ${formatSize(compressedImage.size)}`;
+
+
+    
+    downloadLink.href = compressedURL;
+});
+
+
+
+function formatSize(bytes) {
+
+    if (bytes < 1024 * 1024) {
+        return `${(bytes / 1024).toFixed(1)} KB`;
+    }
+
+    return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
+}
